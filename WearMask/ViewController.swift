364a86ec5060221f4850fa193fa69ebb12e6b1bd
Alexey Antonov <alexeyantonov@me.com>
2020-11-16 16:33:43 +0100

    Initial Commit

---
+++ WearMask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WearMask
+//
+//  Created by Alexey Antonov on 16/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
